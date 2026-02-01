41fc09fa12fea322ed5f375bf28fe9f3b7a96b59
vishal-0810 <vishal.kapale2025@gmail.com>
2026-02-01 01:58:35 +0000

    Restructure & add test & code
    - Restructure the python dir
    - Add tests & code for is_palindrome coding problem
    - Configure uv with pyproject.toml for Python & dependencies

---
+++ python/dsa/tests/test_valid_palindrome.py
@@ -0,0 +1,15 @@
+import pytest
+
+from dsa.code.valid_palindrome import PalindromeChecker
+
+@pytest.mark.parametrize("input_str, expected", [
+    ("A man, a plan, a canal: Panama", True),
+    ("race a car", False),
+    (" ", True),
+    ("", True),
+    ("0P", False),
+    ("ab@a", True),
+])
+def test_is_palindrome(input_str, expected):
+    checker = PalindromeChecker()
+    assert checker.is_palindrome(input_str) == expected
